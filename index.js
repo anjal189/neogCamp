c46b5fc908ef76bcefd8aeda3ef66a976a8ec890
anjal189 <61266918+anjal189@users.noreply.github.com>
2021-12-06 16:53:10 +0530

    Add files via upload
    Solution for ex-12

---
+++ index.js
@@ -0,0 +1,15 @@
+var superman = {
+  name:"superman",
+  power:"flight",
+  costumeColor:"blue",
+  strength:1000,
+}
+
+var batman = {
+  name:"batman",
+  power:"martial arts",
+  costumeColor:"black",
+  strength:100,
+}
+console.log(superman.power)
+console.log(batman.power)
