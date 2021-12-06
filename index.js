c1ef6d5126a9b72eaa40e05d01f1b3e0b27bd1c3
anjal189 <61266918+anjal189@users.noreply.github.com>
2021-12-06 16:55:24 +0530

    Add files via upload
    Solution for ex-13-to-15

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-var superman = {
-  name:"superman",
-  power:"flight",
-  costumeColor:"blue",
-  strength:1000,
+var readlineSync = require("readline-sync");
+score = 0
+
+var userName = readlineSync.question("What is your name ")
+
+console.log("Hii " + userName )
+
+function play(question,answer)
+{
+  var userAns = readlineSync.question(question)
+  if(userAns === answer )
+  {
+    console.log("right")
+    score=score + 1
+  }
+  else{
+    console.log("wrong")
+  }
+  console.log("score is ",score)
 }
 
-var batman = {
-  name:"batman",
-  power:"martial arts",
-  costumeColor:"black",
-  strength:100,
-}
-console.log(superman.power)
-console.log(batman.power)
+play("where do i live ","sangamner")
+play("Fev Colour ","black")
+play("Fev City ","pune")
